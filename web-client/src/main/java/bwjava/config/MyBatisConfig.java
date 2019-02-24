b0ce7617f42ba596f220b43f6481d79d1e5ae074
huang_xiangyu <huang_xiangyu@gzhc365.com>
2019-02-24 11:07:00 +0800

    mybatis 显示注入

---
+++ web-client/src/main/java/bwjava/config/MyBatisConfig.java
@@ -0,0 +1,42 @@
+package bwjava.config;
+
+import com.alibaba.druid.pool.DruidDataSource;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.mybatis.spring.SqlSessionFactoryBean;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
+
+/**
+ * mybatis在某个版本后取消了sqlSessionFactory的自动注入，需要显示注入。
+ *
+ * @author chenjing
+ * @date 2019-02-24 11:04
+ */
+@Configuration
+public class MyBatisConfig {
+
+    @Autowired
+    private DataSourceProperties dataSourceProperties;
+
+    @Bean(name = "dataSource")
+    public DataSource dataSource() {
+
+        DruidDataSource dataSource = new DruidDataSource();
+        dataSource.setUrl(dataSourceProperties.getUrl());
+        dataSource.setDriverClassName(dataSourceProperties.getDriverClassName());
+        dataSource.setUsername(dataSourceProperties.getUsername());
+        dataSource.setPassword(dataSourceProperties.getPassword());
+        return dataSource;
+
+    }
+
+    public SqlSessionFactory sqlSessionFactory() throws Exception {
+        SqlSessionFactoryBean sqlSessionFactoryBean = new SqlSessionFactoryBean();
+        sqlSessionFactoryBean.setDataSource(dataSource());
+        return sqlSessionFactoryBean.getObject();
+    }
+}
